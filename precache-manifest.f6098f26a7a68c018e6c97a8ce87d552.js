0dd11b3a23d67f52a3fa15d7b11c177dcfaf463b
emiljoeantony <emil@designqubearchitects.com>
2020-10-06 16:05:24 +0530

    Updates

---
+++ precache-manifest.f6098f26a7a68c018e6c97a8ce87d552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0eac21708da0cb1e9879b8608c12894",
+    "url": "/React-To-Do-App/index.html"
+  },
+  {
+    "revision": "fb2ad144fe630ba4d0df",
+    "url": "/React-To-Do-App/static/css/main.d7a0e959.chunk.css"
+  },
+  {
+    "revision": "4cd8f0f4759e1132cea3",
+    "url": "/React-To-Do-App/static/js/2.2702f554.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-To-Do-App/static/js/2.2702f554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb2ad144fe630ba4d0df",
+    "url": "/React-To-Do-App/static/js/main.14113f95.chunk.js"
+  },
+  {
+    "revision": "b330f66958b37a31e3f5",
+    "url": "/React-To-Do-App/static/js/runtime-main.403b2f28.js"
+  }
+]);
